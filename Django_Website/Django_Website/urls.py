a7c8a1b4393cb3d4a244bb4c51b7297ec046df37
Najwa Lahly <najwa.lahly@gmail.com>
2020-07-13 21:30:43 +0200

    -add signal.py to create profile -clean main.css and base.htm -modify search bar

--- Django_Website/Django_Website/urls.py
+++ Django_Website/Django_Website/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -29,4 +31,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='Users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='Users/logout.html'), name='logout'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
